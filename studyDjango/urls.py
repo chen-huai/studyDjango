4d9fe6fd21e3fd4dc6588eb041b691f7215b7f55
chenhuai <457794287@qq.com>
2020-12-20 22:06:53 +0800

    数据库模型配置及使用

--- studyDjango/urls.py
+++ studyDjango/urls.py
@@ -1,8 +1,12 @@
 from django.urls import include, re_path
-from . import views
+from . import views,testdb
 urlpatterns = [
     re_path(r'^$', views.hello),
     re_path(r'runoob/', views.runoob),
+    re_path(r'img/', views.runoob),
+    re_path(r'baby/', views.baby),
+    re_path(r'testdb/', testdb.testdb),
+    re_path(r'getData/', testdb.getData),
     # re_path(r'^index/$', views.index, name='index'),
     # re_path(r'^bio/(?P<username>\w+)/$', views.bio, name='bio'),
     # re_path(r'^weblog/', include('blog.urls')),
--- studyDjango/views.py
+++ studyDjango/views.py
@@ -9,3 +9,9 @@ def runoob(request):
     test['hello'] = 'Hello test!'
     views_name = "菜鸟教程"
     return render(request, 'runoob.html', {"name": views_name})
+def img(request):
+    name ="菜鸟教程"
+    return render(request, "runoob.html", {"name": name})
+def baby(request):
+    name ="菜鸟教程"
+    return render(request, "baby.html", {"name": name})
